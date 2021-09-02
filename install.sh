00ce63beb6426e31e93f77d83aae8bb9ccc06423
Vinicius Stock <vinicius.stock@shopify.com>
2021-09-02 14:02:55 -0400

    Configure GPG signing

--- install.sh
+++ install.sh
@@ -4,9 +4,13 @@ if [ $SPIN ]; then
     # Install packages
     sudo apt install -y autoconf coreutils parallel zip bear
 
+    gpgconf --launch dirmngr
+    gpg --keyserver keys.openpgp.org --recv 6BD0EDDB0C455BCE681709061A3EC85374C0969A
+
     # Add git aliases
     git config --global alias.co checkout 
     git config --global alias.sw switch
+    git config --global commit.gpgsign true
 
     # Link dotfiles
     ln -sf ~/dotfiles/zshrc ~/.zshrc
